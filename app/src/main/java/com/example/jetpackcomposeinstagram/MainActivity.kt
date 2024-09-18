1a99f224a98512ec96a6154b59290520462df1bb
Patricia Martínez Espert <pmespert@gmail.com>
2024-09-18 17:03:15 +0200

    LoginScreen Header

--- app/src/main/java/com/example/jetpackcomposeinstagram/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeinstagram/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.jetpackcomposeinstagram.ui.LoginScreen
 import com.example.jetpackcomposeinstagram.ui.theme.JetpackComposeInstagramTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JetpackComposeInstagramTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    LoginScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeInstagramTheme {
-        Greeting("Android")
+        LoginScreen()
     }
 }
